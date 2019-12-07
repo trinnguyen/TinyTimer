fafb8829a5b980bbc0e6f1d664cd16ff962eaf59
Tri Nguyen <tringuyen.nnt@gmail.com>
2019-12-07 17:12:34 +0100

    Support Dark Mode

--- TinyTimer/Builds.swift
+++ TinyTimer/Builds.swift
@@ -9,7 +9,17 @@
 import Cocoa
 
 class Builds: NSObject {
-    static var normalColor = NSColor.black
+    static var normalColor: NSColor = isDarkMode() ? NSColor.white : NSColor.black
     static var highlightColor = NSColor.init(calibratedRed: 15.0/255.0, green: 175.0/255.0, blue: 104.0/255.0, alpha: 1)
     static var defaultTimers = 60 * 30 //in seconds
+    
+    static func isDarkMode() -> Bool {
+        if #available(macOS 10.14, *) {
+           if NSAppearance.current.bestMatch(from: [.darkAqua, .aqua]) == .darkAqua {
+              return true
+           }
+         }
+
+         return false
+    }
 }
